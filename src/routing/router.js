40c3b06c9af323a68ed50ed89a3c967e090c4080
Elvir Vlahovljak <46258494+ElvaRivle@users.noreply.github.com>
2023-05-16 10:58:34 +0200

    ElvaRivle/Feature/Sidebar (#8)
    * FEATURE/ElvaRivle/Sidebar
    
    --------------------------------------------------------------------
     MAIN
     * added Sidebar component
     * added temporary route only for sidebar
     * added ant-design icons package
     * added all resources neccessary for sidebar
    --------------------------------------------------------------------
     OTHER
     * added text editor config files to .gitignore
    --------------------------------------------------------------------
     TODO
     * add styling to component
    
    * ElvaRivle/Feature/Sidebar
    
    --------------------------------------------------------------------
     MAIN
     * added styling to sidebar
    --------------------------------------------------------------------
     OTHER
     * /
    --------------------------------------------------------------------
    
    * ElvaRivle/Feature/Sidebar
    
    --------------------------------------------------------------------
     MAIN
     * added font mixin
     * colors moved to _colors.scss
     * created array of sidebar options and properly displayed it
     * import order fixed
    --------------------------------------------------------------------
     OTHER
     * /
    --------------------------------------------------------------------
    
    * ElvaRivle/Feature/Sidebar
    
    --------------------------------------------------------------------
     MAIN
     * removed leftover mixin
    --------------------------------------------------------------------
     OTHER
     * /
    --------------------------------------------------------------------
    
    * FEATURE/ElvaRivle/Sidebar
    
    --------------------------------------------------------------------
     MAIN
     * fixed font mixins
     * moved `sidebarOptions.map` outside JSX
    --------------------------------------------------------------------
     OTHER
     * Google fonts import URL moved to Sidebar.scss because it didn't load in index.css
     * small bottom margin for sidebar options
    --------------------------------------------------------------------
    
    * FEATURE/ElvaRivle/Sidebar
    
    --------------------------------------------------------------------
     MAIN
     * protected /sidebar route
    --------------------------------------------------------------------
     OTHER
     * for the beginning, only basic "is accessToken present" check is implemented
    --------------------------------------------------------------------
    
    * ElvaRivle/Feature/Sidebar
    
    --------------------------------------------------------------------
     MAIN
     * created wrapper component protectedRoutes that implements core authN/authZ logic
     * every set of protected routes should be child of that protectedRoutes component
     * created single index route for dashboard routes
    --------------------------------------------------------------------
     OTHER
     * created empty Dashboard component
     * default route is not /login, but /dashboard, redirect to login will happen if user not authN/authZ
    --------------------------------------------------------------------

--- src/routing/router.js
+++ src/routing/router.js
@@ -1,17 +1,24 @@
 import React from "react";
 import LoginForm from '../components/loginform/modules/index.js';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+//FIXME: something tells me I shouldn't import sidebar like this!?
+import Sidebar from "../components/sidebar/Sidebar";
+import ProtectedRoutes from "./protectedRoutes";
+import Dashboard from "../components/dashboard/Dashboard";
+import DashboardRoutes from "./dashboardRoutes";
 
 function CustomRouter() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="/login" element={<LoginForm />} />
-        <Route path="*" element={<Navigate to="/login" />} />
+        <Route path="/" element={<Navigate to="/dashboard" />} />
+        <Route exact path="/login" element={<LoginForm />} />
+        <Route path="*" element={<Navigate to="/dashboard" />} />
+
+        <Route path = "/dashboard/*" element={<DashboardRoutes />} />
       </Routes>
     </Router>
-  );
+  )
 }
 
 export default CustomRouter;
