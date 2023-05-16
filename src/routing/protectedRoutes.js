f01e9da71404a768cfa9590ff304b73b85e08f14
Elvir Vlahovljak <elvirvlahovljak@gmail.com>
2023-05-16 10:48:19 +0200

    ElvaRivle/Feature/Sidebar
    --------------------------------------------------------------------
     MAIN
     * created wrapper component protectedRoutes that implements core authN/authZ logic
     * every set of protected routes should be child of that protectedRoutes component
     * created single index route for dashboard routes
    --------------------------------------------------------------------
     OTHER
     * created empty Dashboard component
     * default route is not /login, but /dashboard, redirect to login will happen if user not authN/authZ
    --------------------------------------------------------------------

--- src/routing/protectedRoutes.js
+++ src/routing/protectedRoutes.js
@@ -1,6 +1,22 @@
-import { Navigate, Outlet } from "react-router-dom";
+import {Navigate, Outlet, Route, Routes} from "react-router-dom";
+import Sidebar from "../components/sidebar/Sidebar";
+import Dashboard from "../components/dashboard/Dashboard";
+import React from "react";
 
-export default function ProtectedRoutes() {
+const ProtectedRoutes = () => {
+  //here is the core logic that prevents unauthorized access to routes
+  //each protected route should have this component as parent
+
   let token = localStorage.getItem("accessToken")
-  return token && token !== "" ? <Outlet /> : <Navigate to="/login" />;
+
+  return token && token !== "" ?
+    (
+      <>
+        <Sidebar />
+        <Outlet />
+      </>
+    )
+  : <Navigate to="/login" />
 }
+
+export default ProtectedRoutes
--- src/routing/router.js
+++ src/routing/router.js
@@ -4,18 +4,18 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 //FIXME: something tells me I shouldn't import sidebar like this!?
 import Sidebar from "../components/sidebar/Sidebar";
 import ProtectedRoutes from "./protectedRoutes";
+import Dashboard from "../components/dashboard/Dashboard";
+import DashboardRoutes from "./dashboardRoutes";
 
 function CustomRouter() {
   return (
     <Router>
       <Routes>
-        <Route element={<ProtectedRoutes />}>
-          <Route path="/sidebar" element={<Sidebar />} />
-        </Route>
+        <Route path="/" element={<Navigate to="/dashboard" />} />
+        <Route exact path="/login" element={<LoginForm />} />
+        <Route path="*" element={<Navigate to="/dashboard" />} />
 
-        <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="/login" element={<LoginForm />} />
-        <Route path="*" element={<Navigate to="/login" />} />
+        <Route path = "/dashboard/*" element={<DashboardRoutes />} />
       </Routes>
     </Router>
   )
