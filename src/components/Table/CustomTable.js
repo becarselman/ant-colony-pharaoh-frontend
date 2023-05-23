03d2e3a9ef54874300825b53170b702fdfc732a3
TarikVelic <tarik.velic@ses.edu.ba>
2023-05-23 19:02:27 +0200

    TariikMVP/FEATURE/ProjectsTable
    --------------------------------------------------------------------
     MAIN
     * Review fixes
    --------------------------------------------------------------------
     OTHER
     * <other msg>
    --------------------------------------------------------------------

--- src/components/Table/CustomTable.js
+++ src/components/Table/CustomTable.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { Table, Pagination, Select } from 'antd';
+import { Table } from 'antd';
+import PaginationComponent from './Pagination';
 import './CustomTable.scss';
 
 const CustomTable = ({ data, columns }) => {
@@ -21,52 +22,19 @@ const CustomTable = ({ data, columns }) => {
   const endIndex = startIndex + pageSize;
   const pageData = data.slice(startIndex, endIndex);
 
-  const paginationConfig = {
-    pageSize: pageSize,
-    total: totalItems,
-    current: currentPage,
-    onShowSizeChange: handlePageSizeChange,
-    onChange: handlePageChange,
-    showTotal: (total, range) => `${range[0]}-${range[1]} of ${total} items`,
-  };
-
-  const handleNextPage = () => {
-    if (currentPage < Math.ceil(totalItems / pageSize)) {
-      setCurrentPage(currentPage + 1);
-    }
-  };
-
-  const handleLastPage = () => {
-    setCurrentPage(Math.ceil(totalItems / pageSize));
-  };
-
   return (
-    <div className="table-container">
-      <Table dataSource={pageData} columns={columns} className="table" pagination={false} />
-
+    <div className="custom-table-container">
+      <div className="table-container">
+        <Table dataSource={pageData} columns={columns} className="table" pagination={false} />
+      </div>
       <div className="pagination-container">
-        <div className="pagination-info">
-          <span className="rows-text">Rows per page:</span>
-          <Select value={pageSize.toString()} onChange={handlePageSizeChange}>
-            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((value) => (
-              <Select.Option key={value} value={value.toString()}>
-                {value}
-              </Select.Option>
-            ))}
-          </Select>
-          <span className="show-total">{paginationConfig.showTotal(totalItems, [startIndex + 1, endIndex])}</span>
-        </div>
-        <div className="pagination-right">
-        <Pagination {...paginationConfig} className="custom-pagination" showQuickJumper={false} />
-        <div className="next-last-buttons">
-          <button onClick={handleNextPage} disabled={currentPage === Math.ceil(totalItems / pageSize)}>
-            Next
-          </button>
-          <button onClick={handleLastPage} disabled={currentPage === Math.ceil(totalItems / pageSize)}>
-            Last
-          </button>
-          </div>
-        </div>
+        <PaginationComponent
+          totalItems={totalItems}
+          pageSize={pageSize}
+          currentPage={currentPage}
+          onPageChange={handlePageChange}   
+          onPageSizeChange={handlePageSizeChange}
+        />
       </div>
     </div>
   );
---
+++ src/components/Table/Pagination.js
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+import { Pagination, Select } from 'antd';
+
+const PaginationComponent = ({ totalItems, pageSize, currentPage, onPageChange, onPageSizeChange }) => {
+  const handlePageSizeChange = (value) => {
+    onPageSizeChange(parseInt(value));
+  };
+
+  const handlePageChange = (page) => {
+    onPageChange(page);
+  };
+
+  const startIndex = (currentPage - 1) * pageSize;
+  const endIndex = startIndex + pageSize;
+
+  const paginationConfig = {
+    pageSize: pageSize,
+    total: totalItems,
+    current: currentPage,
+    onShowSizeChange: handlePageSizeChange,
+    onChange: handlePageChange,
+    showTotal: (total, range) => `${range[0]}-${range[1]} of ${total} Projects`,
+  };
+
+  const handleNextPage = () => {
+    if (currentPage < Math.ceil(totalItems / pageSize)) {
+      onPageChange(currentPage + 1);
+    }
+  };
+
+  const handleLastPage = () => {
+    onPageChange(Math.ceil(totalItems / pageSize));
+  };
+
+  const isLastPage = currentPage === Math.ceil(totalItems / pageSize);
+
+  const pageSizeSelect = (
+    <Select value={pageSize.toString()} onChange={handlePageSizeChange}>
+      {['5', '10', '20'].map((value) => (
+        <Select.Option key={value} value={value}>
+          {value}
+        </Select.Option>
+      ))}
+    </Select>
+  );
+
+  const showTotalSpan = (
+    <span className="show-total">{paginationConfig.showTotal(totalItems, [startIndex + 1, endIndex])}</span>
+  );
+
+  const nextButton = (
+    <button onClick={handleNextPage} disabled={isLastPage}>
+      Next
+    </button>
+  );
+
+  const lastButton = (
+    <button onClick={handleLastPage} disabled={isLastPage}>
+      Last
+    </button>
+  );
+
+  return (
+    <div className="pagination-container">
+      <div className="pagination-info">
+        <span className="rows-text">Rows per page:</span>
+        {pageSizeSelect}
+        {showTotalSpan}
+      </div>
+      <div className="pagination-right">
+        <Pagination {...paginationConfig} className="custom-pagination" />
+        <div className="next-last-buttons">
+          {nextButton}
+          {lastButton}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PaginationComponent;
