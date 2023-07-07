292ab1b6f0770fc35e12b5aeabc5c98510b1517a
TarikVelic <tarik.velic@ses.edu.ba>
2023-07-07 12:41:21 +0200

    Switched from popover to tooltip

--- src/components/projects/components/Avatar.js
+++ src/components/projects/components/Avatar.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Avatar, Popover } from 'antd';
+import { Avatar, Tooltip } from 'antd';
 
-const AvatarComponent = ({ name, developerList }) => {
+const AvatarComponent = ({ name,}) => {
   const generateColor = (name) => {
     let hash = 0;
     for (let i = 0; i < name.length; i++) {
@@ -15,9 +15,9 @@ const AvatarComponent = ({ name, developerList }) => {
   const darkenColor = (color, percent) => {
     const num = parseInt(color.slice(1), 16);
     const amt = Math.round(2.55 * percent);
-    const R = (num >> 16) - amt;
-    const G = (num >> 8 & 0x00FF) - amt;
-    const B = (num & 0x0000FF) - amt;
+    const R = ((num >> 16) - amt);
+    const G = (((num >> 8) & 0x00FF) - amt);
+    const B = ((num & 0x0000FF) - amt);
     const newColor = "#" + (0x1000000 + (R < 255 ? (R < 1 ? 0 : R) : 255) * 0x10000 + (G < 255 ? (G < 1 ? 0 : G) : 255) * 0x100 + (B < 255 ? (B < 1 ? 0 : B) : 255)).toString(16).slice(1);
     return newColor;
   };
@@ -29,7 +29,6 @@ const AvatarComponent = ({ name, developerList }) => {
       .join('');
     return (
       <Avatar
-        className=""
         style={{
           backgroundColor: color,
           backgroundImage: `linear-gradient(to bottom right, ${color}, ${darkenColor(color, 40)})`,
@@ -43,15 +42,16 @@ const AvatarComponent = ({ name, developerList }) => {
   const renderDeveloperAvatar = (developer, developers) => {
     const color = generateColor(developer);
     return (
-      <span className="avatar-container">
-        <Popover
-          content={developers.join(', ')}
+      <span className="avatar-container" key={developer}>
+        <Tooltip
+          title={developers.join(', ')}
+          color='black'
           key={developer}
           overlayClassName="popover-class"
           overlayStyle={{ width: 'fit-content', }}
         >
           {renderAvatar(developer, color)}
-        </Popover>
+        </Tooltip>
       </span>
     );
   };
